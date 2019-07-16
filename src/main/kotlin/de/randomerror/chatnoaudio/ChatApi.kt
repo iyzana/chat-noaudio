09af5e786d78ce8639f043515685bd37c80d73d0
succcubbus <jannis.kaiser2@gmail.com>
2019-07-16 22:40:08 +0200

    extract last message type and unread status

--- src/main/kotlin/de/randomerror/chatnoaudio/ChatApi.kt
+++ src/main/kotlin/de/randomerror/chatnoaudio/ChatApi.kt
@@ -8,6 +8,9 @@ import com.google.zxing.common.HybridBinarizer
 import com.google.zxing.qrcode.QRCodeReader
 import com.google.zxing.qrcode.QRCodeWriter
 import org.openqa.selenium.By
+import org.openqa.selenium.NoSuchElementException
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.firefox.GeckoDriverService
 import java.io.ByteArrayInputStream
@@ -15,10 +18,16 @@ import java.io.File
 import java.util.*
 import javax.imageio.ImageIO
 
-data class Chat(val name: String, val cssClass: String)
+data class Chat(val name: String, val elementIndex: Int, val hasUnread: Boolean, val lastMessageType: MessageType)
+
+enum class MessageType {
+    TEXT,
+    AUDIO,
+    OTHER
+}
 
 class ChatApi {
-    private val driver = GeckoDriverService.Builder()
+    private val driver: WebDriver = GeckoDriverService.Builder()
         .usingDriverExecutable(File(System.getenv("HOME") + "/Downloads/geckodriver"))
         .build()
         .let(::FirefoxDriver)
@@ -58,32 +67,65 @@ class ChatApi {
     fun getChats(): Set<Chat> {
         return driver.findElement(By.id("pane-side"))
             .findElements(By.xpath("./div/div/div/div"))
-            .map { chatElement ->
-                val name = chatElement.findElement(By.xpath("./div[1]/div[1]/div[2]/div/div/span")).text
-                val cssClass = chatElement.getAttribute("class")
-                // todo: find unique html identifier
-                Chat(name, "")
+            .mapIndexed { index, chatElement ->
+                val name = chatElement.findElement(By.xpath("./div[1]/div[1]/div[2]/div[1]/div[1]/span")).text
+                val hasUnread = hasUnreadMessages(chatElement)
+                val messageType = getMessageType(chatElement)
+                Chat(name, index + 1, hasUnread, messageType)
             }
             .toSet()
     }
 
     fun hasNewAudioMessage(chat: Chat): Boolean {
-        val chatElement = driver.findElementByClassName(chat.cssClass)
-        // todo: parse new messages icon and last message type icon
-        return false
+        return chat.lastMessageType == MessageType.AUDIO
     }
 
     fun getAudioMessage(chat: Chat): ByteArray {
-        val chatElement = driver.findElementByClassName(chat.cssClass)
-        chatElement.click()
+        getChat(chat).click()
+        Thread.sleep(100)
+
+        val audioSource = driver.findElement(By.id("main"))
+            .waitForElement(By.xpath("./div[3]/div[1]/div[1]/div[3]/div[last()]"), driver)
+            .waitForElement(By.xpath("./div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/audio"), driver)
+            .getAttribute("src")
+
+        driver.get(audioSource)
 
         return "audio".toByteArray()
     }
 
     fun sendMessage(chat: Chat, text: String) {
-        val chatElement = driver.findElementByClassName(chat.cssClass)
+        val chatElement = getChat(chat)
         chatElement.click()
         // todo: find text input and send keys
+    }
+
+    private fun getChat(chat: Chat): WebElement {
+        return driver.findElement(By.id("pane-side"))
+            .findElement(By.xpath("./div[1]/div[1]/div[1]/div[${chat.elementIndex}]"))
+    }
+
+    private fun getMessageType(chatElement: WebElement): MessageType {
+        return try {
+            val attribute =
+                chatElement.findElement(By.xpath("./div[1]/div[1]/div[2]/div[2]/div[1]/span[1]/div/span"))
+                    .getAttribute("data-icon")
+            when (attribute) {
+                "status-ptt-green" -> MessageType.AUDIO
+                "status-ptt-blue" -> MessageType.AUDIO
+                else -> MessageType.OTHER
+            }
+        } catch (e: NoSuchElementException) {
+            MessageType.TEXT
+        }
+    }
+
+    private fun hasUnreadMessages(chatElement: WebElement): Boolean {
+        val unreadMessages =
+            chatElement.findElements(By.xpath("./div[1]/div[1]/div[2]/div[2]/div[2]/span[1]/div/span"))
+                .mapNotNull { it.text.toIntOrNull() }
+                .firstOrNull()
+        return unreadMessages != null
     }
 
     fun close() {
